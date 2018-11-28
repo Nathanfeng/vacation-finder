99126931d3c46d605ee4f269c961561ca4262c1e
Nathan Feng <nathanlfeng@gmail.com>
2018-11-27 19:46:05 -0500

    added in bootstrap and form

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs')
 
-var campgrounds = [
+var vacationSpots = [
   {name: "first", image: "https://images.pexels.com/photos/6534/holiday-vacation-hotel-luxury.jpg?auto=compress&cs=tinysrgb&h=350"},
   {name: 'second', image: 'https://images.pexels.com/photos/1024992/pexels-photo-1024992.jpeg?auto=compress&cs=tinysrgb&h=350'},
   {name:'third', image: 'https://images.pexels.com/photos/6534/holiday-vacation-hotel-luxury.jpg?auto=compress&cs=tinysrgb&h=350'}
@@ -13,8 +15,19 @@ app.get('/', (req, res) => {
   res.render('landing');
 })
 
-app.get('/campgrounds', (req, res) =>{
-  res.render('campgrounds', {campgrounds: campgrounds});
+app.get('/vacationSpots', (req, res) =>{
+  res.render('vacationSpots', {vacationSpots: vacationSpots});
+})
+
+app.get('/vacationSpots/new', (req, res) => {
+  res.render('new.ejs');
+})
+
+app.post('/vacationSpots', (req, res) => {
+  let newSpot = req.body.name;
+  let newSpotPicture = req.body.image;
+  vacationSpots.push({name: newSpot, image: newSpotPicture});
+  res.redirect('/vacationSpots');
 })
 
 app.listen(3000, () => {
